b007dd33fdbf65c65ebe36a8273ad068edd3f948
Nguyen Quoc Dat <nqd.1912@gmail.com>
2022-03-27 15:27:27 +0700

    Implement Exception filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HttpExceptionFilter } from './common/exception_filter/http-exception.filter';
 import { LoggerMiddleware } from './middleware/logger.middleware';
 import { CustomerModule } from './module/customer/customer.module';
 import { DivisionModule } from './module/division/division.module';
@@ -10,7 +12,10 @@ import { UserModule } from './module/user/user.module';
 @Module({
   imports: [UserModule, CustomerModule, DivisionModule, ProjectModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, {
+    provide: APP_FILTER,
+    useClass: HttpExceptionFilter,
+  }],
 })
 
 export class AppModule implements NestModule {
